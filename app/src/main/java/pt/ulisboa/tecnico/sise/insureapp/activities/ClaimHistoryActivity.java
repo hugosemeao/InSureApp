7af0cb297aae63c0cdc796ea5cf3f4c2e4069ded
hsemeao <hmsemeao@gmail.com>
2020-04-03 19:27:03 +0100

    3.04 19:26

--- app/src/main/java/pt/ulisboa/tecnico/sise/insureapp/activities/ClaimHistoryActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/sise/insureapp/activities/ClaimHistoryActivity.java
@@ -1,7 +1,12 @@
 package pt.ulisboa.tecnico.sise.insureapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -11,12 +16,34 @@ import pt.ulisboa.tecnico.sise.insureapp.R;
 import pt.ulisboa.tecnico.sise.insureapp.datamodel.ClaimItem;
 
 public class ClaimHistoryActivity extends AppCompatActivity {
-    private ArrayList<ClaimItem> _claimList;
+    public final static String TAG = "ClaimHistory";
+    private List<ClaimItem> _claimList;
     private ListView _listView;
+    public Integer sessionID=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_claim_history);
+        _listView = (ListView) findViewById(R.id.list_claims_list);
+
+        //place the claim list in the application domain
+        _claimList= new ListClientClaimsTask.execute(sessionID,_listView);
+
+        //assign adapter to listView
+        ArrayAdapter<ClaimItem> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, _claimList);
+        _listView.setAdapter(adapter);
+
+        // attach click listener to list view items
+        _listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // create the read note activity, passing to it the index position as parameter
+                Log.d("position", position+"");
+
+
+
     }
 }
