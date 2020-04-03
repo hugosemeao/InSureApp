8ce671cd50f223445b834ab5a8694a87f23dc1bd
hsemeao <hmsemeao@gmail.com>
2020-04-03 16:01:01 +0100

    With WS

--- app/src/main/java/pt/ulisboa/tecnico/sise/insureapp/ClaimHistoryActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/sise/insureapp/ClaimHistoryActivity.java
@@ -1,8 +1,17 @@
 package pt.ulisboa.tecnico.sise.insureapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pt.ulisboa.tecnico.sise.insureapp.datamodel.ClaimItem;
 
 public class ClaimHistoryActivity extends AppCompatActivity {
+    private ArrayList<ClaimItem> _claimList;
+    private ListView _listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
