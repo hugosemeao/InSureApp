c9dd1f49adfd93a0758fcfb4b0d15a816804a4e4
corina-8 <cf.mata@campus.fct.unl.pt>
2020-04-05 17:31:09 +0100

    05/04

--- app/src/main/java/pt/ulisboa/tecnico/sise/insureapp/activities/CustomerInformationActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/sise/insureapp/activities/CustomerInformationActivity.java
@@ -1,14 +1,31 @@
 package pt.ulisboa.tecnico.sise.insureapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import pt.ulisboa.tecnico.sise.insureapp.R;
 
+
+
 public class CustomerInformationActivity extends AppCompatActivity {
+    private static final String TAG = "ViewCustomerInformation";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_information);
     }
-}
+
+            public void onClick(View view) {
+            Log.d(TAG, "Back Button Clicked");
+            Intent intent = new Intent(CustomerInformationActivity.this, MainMenuActivity.class);
+            startActivity(intent);
+        }
+    }
+
+
+
+
